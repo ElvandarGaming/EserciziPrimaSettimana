70ebd206b0f112a7bccbdd453c682ef49855acb0
LucaB <LucaB@DESKTOP-9ILK5NC.homenet.telecomitalia.it>
2021-07-04 20:24:08 +0200

    import pc

--- EserciziPrimaSettimana/src/banca/Cliente.java
+++ EserciziPrimaSettimana/src/banca/Cliente.java
@@ -1,5 +1,12 @@
 package banca;
+import java.time.LocalDate;
+
 
 public class Cliente {
-
+	public String nome;
+	public String cognome;
+	public String CodFiscale;
+	public String cellNumber;
+	public ContoCorrente[] conti = new ContoCorrente[3];
+	//	for(int i = 0);
 }
---
+++ EserciziPrimaSettimana/src/epicode/RandomTry.java
@@ -0,0 +1,10 @@
+package epicode;
+
+public class RandomTry {
+
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+
+	}
+
+}
